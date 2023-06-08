c762e4f91b6562d112a2d903beca407289e21a96
Mokshith321 <95397131+Mokshith321@users.noreply.github.com>
2023-06-08 19:23:09 +0530

    Add files via upload

--- bookart/settings.py
+++ bookart/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 
 # STATICFILES_DIRS =  [
@@ -150,5 +150,5 @@ MEDIA_URL = 'media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
